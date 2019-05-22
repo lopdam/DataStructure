5d017ff9de3c3936041e1ebde4346551dd6e8d9f
lopdam <dennyslopez2013@gmail.com>
2019-05-22 07:14:12 -0500

    Invertir Arreglo

--- Recursive/InvertirArreglo.java
+++ Recursive/InvertirArreglo.java
@@ -9,9 +9,32 @@ public class InvertirArreglo{
 
   public static void main(String[] args) {
 
+    int arreglo[] = {1, 2, 3, 4};
+
+    System.out.println(Invertir(arreglo,0, arreglo.length-1).toString());
+
   }
 
-  public static int[] Invertir(int arreglo[]){
-    
+  public static int[] Invertir(int arreglo[], int low, int high) {
+
+      int num1=arreglo[low];
+      int num2=arreglo[high];
+
+      if ((low + 1) == (high)) {
+        arreglo[low]=num1;
+        arreglo[high]=num2;
+        return arreglo;
+        //  return arreglo[low] + arreglo[high];
+      } else if (low == high) {
+          arreglo[low]==num1;
+          return arreglo;
+          //return arreglo[low];
+      }
+
+      arreglo[low]=num1;
+      arreglo[high]=num2;
+
+      return Invertir(arreglo, ++low, --high);
+
   }
 }
